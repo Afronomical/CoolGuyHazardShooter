67cb664e3b6031cb7f28327b408488dc6341f0d8
Afronaut <66644410+Afronomical@users.noreply.github.com>
2024-10-11 15:05:14 +0100

    Added SDL

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
-
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
     std::cout << "Hello World!\n";
+    SDL_Init(SDL_INIT_EVERYTHING);
+    TTF_Init();
+    Mix_Init(MIX_INIT_MP3);
+    IMG_Init(IMG_INIT_PNG);
+
+    return 0;
 }
 
