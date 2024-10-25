e52f90dc1b1f6f10036936ee3ea00461078fe90f
TheMGLegends <matejovo34@gmail.com>
2024-10-25 23:05:50 +0100

    Renamed folder to debugging so it shows up on github

---
+++ Source/Debugging/Debug.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+
+//#include "../Collision/BoxCollider.h"
+//#include "../Collision/CircleCollider.h"
+#include "../Color/Color.h"
+#include "../Vector2/Vector2.h"
+
+class SDL_Renderer;
+
+static class Debug
+{
+public:
+	//static void DrawColliderOutline(const BoxCollider& collider, const Color& color);
+	//static void DrawColliderOutline(const CircleCollider& collider, const Color& color);
+	static void DrawLine(const Vector2& start, const Vector2& end, const Color& color);
+
+	static inline void Log(std::string message) { std::cout << message << std::endl; }
+	static void LogWarning(std::string message);
+	static void LogError(std::string message);
+
+
+	static inline void SetRenderer(SDL_Renderer* _renderer) { renderer = _renderer; }
+
+private:
+	static SDL_Renderer* renderer;
+};
+
