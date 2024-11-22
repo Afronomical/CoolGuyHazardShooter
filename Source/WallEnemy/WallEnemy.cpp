d0376b10e9a926ac9dfe5fc1de543bcb1816c781
MaxP <max.piper.uk@gmail.com>
2024-11-22 11:05:04 +0000

    wallenemy start (box png is a placeholder lol)

---
+++ Source/WallEnemy/WallEnemy.cpp
@@ -0,0 +1,27 @@
+#include "WallEnemy.h"
+
+
+WallEnemy::WallEnemy()
+{
+	wallTexture = AssetHandler::LoadTexture("Assets/Animations/rb_305.png");
+}
+
+WallEnemy::~WallEnemy()
+{
+
+}
+
+void WallEnemy::Update(float deltaTime)
+{
+
+}
+
+void WallEnemy::Draw()
+{
+	AssetHandler::DrawAnimation(
+		wallTexture,
+		transform.lock()->position,
+		transform.lock()->size.X,
+		transform.lock()->size.Y,
+		1, 1);
+}
